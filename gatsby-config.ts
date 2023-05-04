1a005acbff84242748bfca63f9e83c69ddc1d3c3
Tharon Carlson <tharon.carlson@healthtrio.com>
2023-05-04 12:54:20 -0400

    Move blog files to src directory
    - Update gatstby-source-filesystem to point to new blog location
    - Renamed blog files to src directory

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -16,7 +16,7 @@ const config: GatsbyConfig = {
       resolve: 'gatsby-source-filesystem',
       options: {
         "name": "blog",
-        "path": `${__dirname}/blog`
+        "path": `${__dirname}/src/blog`
       },
       __key: "pages"
     },
